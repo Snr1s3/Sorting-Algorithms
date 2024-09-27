44bd16c580cfed11cab8241fc14a6275a299a4b0
Snr1s3 <m1ss.sunrse@gmail.com>
2024-09-27 23:41:39 +0200

    update

--- Selection-sort/c/SelectionSort.c
+++ Selection-sort/c/SelectionSort.c
@@ -2,12 +2,20 @@
 
 void selectionSort(int arr[], int n)
 {
-    int min;
     int temp;
-    for(int i = 0; i < n-1; j++)
+    for(int i = 0; i < n-1; i++)
     {
         int min = i;
-        for(int j = i + 1; j < n;)
+        for(int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min]) 
+            {
+                min = j;
+            }
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
     }
 
 }
